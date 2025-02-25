901b67acd40c944c325dbf284ece2f43173cb7aa
bestia.dev <info@bestia.dev>
2025-02-25 20:42:56 +0000

    update docs

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["oauth2_cli_github_example",["",[],["crates_io_api_token.rs","github_oauth2_device_workflow_mod.rs","main.rs"]]],["oauth2_cli_github_example",["",[],["crates_io_api_token_mod.rs","encrypt_decrypt_with_ssh_key_mod.rs","github_api_token_with_oauth2_mod.rs","main.rs"]]],["oauth2_cli_github_example",["",[],["crates_io_api_token_mod.rs","encrypt_decrypt_with_ssh_key_mod.rs","github_oauth2_device_workflow_mod.rs","main.rs"]]],["oauth2_cli_github_example",["",[],["crates_io_api_token_mod.rs","encrypt_decrypt_with_ssh_key_mod.rs","main.rs"]]],["oauth2_cli_github_example",["",[],["github_oauth2_device_workflow_mod.rs","main.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["oauth2_cli_github_example",["",[],["crates_io_api_token_mod.rs","encrypt_decrypt_with_ssh_key_mod.rs","github_api_token_with_oauth2_mod.rs","main.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[113,155,156,117,89]}
+//{"start":36,"fragment_lengths":[154]}
--- docs/trait.impl/zeroize/trait.Zeroize.js
+++ docs/trait.impl/zeroize/trait.Zeroize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["oauth2_cli_github_example",[["impl Zeroize for <a class=\"struct\" href=\"oauth2_cli_github_example/github_api_token_with_oauth2_mod/struct.ResponseSecretAccessToken.html\" title=\"struct oauth2_cli_github_example::github_api_token_with_oauth2_mod::ResponseSecretAccessToken\">ResponseSecretAccessToken</a>"]]],["oauth2_cli_github_example",[["impl Zeroize for <a class=\"struct\" href=\"oauth2_cli_github_example/github_oauth2_device_workflow_mod/struct.ResponseSecretAccessToken.html\" title=\"struct oauth2_cli_github_example::github_oauth2_device_workflow_mod::ResponseSecretAccessToken\">ResponseSecretAccessToken</a>"]]]]);
+    var implementors = Object.fromEntries([["oauth2_cli_github_example",[["impl Zeroize for <a class=\"struct\" href=\"oauth2_cli_github_example/github_api_token_with_oauth2_mod/struct.ResponseSecretAccessToken.html\" title=\"struct oauth2_cli_github_example::github_api_token_with_oauth2_mod::ResponseSecretAccessToken\">ResponseSecretAccessToken</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[312,315]}
+//{"start":57,"fragment_lengths":[312]}
